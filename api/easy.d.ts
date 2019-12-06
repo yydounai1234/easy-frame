04d1c83b32deca49c3722c7569ec0ee8f0ec1610
tongjie <794095300@qq.com>
2019-12-06 16:30:50 +0800

    test

---
+++ api/easy.d.ts
@@ -0,0 +1,58 @@
+
+declare interface Component {
+    render: string;
+    methods?: object;
+    data?: object;
+    type: string;
+}
+
+export declare function createApp(app: Component, container: Element): void;
+
+declare type DebuggerEvent = {
+    effect: ReactiveEffect;
+    target: object;
+    type: OperationTypes;
+    key: any;
+} & DebuggerEventExtraInfo;
+
+declare interface DebuggerEventExtraInfo {
+    newValue?: any;
+    oldValue?: any;
+    oldTarget?: Map<any, any> | Set<any>;
+}
+
+declare type Dep = Set<ReactiveEffect>;
+
+export declare function effect<T = any>(fn: () => T, options?: ReactiveEffectOptions): ReactiveEffect<T>;
+
+declare const enum OperationTypes {
+    SET = "set",
+    ADD = "add",
+    DELETE = "delete",
+    CLEAR = "clear",
+    GET = "get",
+    HAS = "has",
+    ITERATE = "iterate"
+}
+
+export declare function reactive(target: object): any;
+
+declare interface ReactiveEffect<T = any> {
+    (): T;
+    _isEffect: true;
+    active: boolean;
+    raw: () => T;
+    deps: Array<Dep>;
+    options: ReactiveEffectOptions;
+}
+
+declare interface ReactiveEffectOptions {
+    lazy?: boolean;
+    computed?: boolean;
+    scheduler?: (run: Function) => void;
+    onTrack?: (event: DebuggerEvent) => void;
+    onTrigger?: (event: DebuggerEvent) => void;
+    onStop?: () => void;
+}
+
+export { }
